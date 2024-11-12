54212d70172f0c130cb378cc8bd02c29116c4da4
pavlo.chubarov <pavlo.chubarov@infotech.gov.ua>
2024-11-12 09:20:39 +0200

    update b car_model admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
 
 from main.models import Customer, CustomerRequest
 
@@ -25,11 +26,16 @@ class CustomerAdmin(admin.ModelAdmin):
 @admin.register(CustomerRequest)
 class CustomerRequestAdmin(admin.ModelAdmin):
     list_display = [
-        'request_type', 'car_model', 'car_vin', 'car_part_name', 'car_city', 'customer_name', 'customer', 'status', 'created'
+        'request_type', 'car_model_b', 'car_vin', 'car_part_name', 'car_city', 'customer_name', 'customer', 'status', 'created'
     ]
-    list_display_links = ('request_type', 'car_model')
+    list_display_links = ('request_type', 'car_model_b')
     list_filter = ['created', 'status', 'request_type']
     search_fields = ['car_part_name', 'car_vin', 'customer__user__username']
+
+    def car_model_b(self, obj):
+        return mark_safe('<b>%s</b>' % obj.car_model)
+    car_model_b.short_description = 'Car Model'
+    car_model_b.allow_tags = True
 
 
 admin.site.unregister(User)
